0184ca980a6bc4b6f2cc7e5ebdba34541b8c9989
Shabbir Bawaji <bawaji94@gmail.com>
2023-01-04 17:09:24 +0530

    move output array calculation to spectral data

--- Colors.py
+++ Colors.py
@@ -1,5 +1,4 @@
 from Color import Color
-import numpy as np
 
 
 class Colors(list):
@@ -25,17 +24,8 @@ class Colors(list):
             colors = colors + [red]
         return colors
 
-    def convert_data(self, spectral_data, output_array_shape, slice_before, slice_after):
-        output_array = np.zeros(output_array_shape)
-        bands = reversed(range(self.__no_of_bands)) if self.__inverse else range(self.__no_of_bands)
-        red_band = slice_before + (0,) + slice_after
-        green_band = slice_before + (1,) + slice_after
-        blue_band = slice_before + (2,) + slice_after
-        for color, band_no in zip(self, bands):
-            rgb = color.rgb()
-            data_at_spectra = spectral_data[slice_before + (band_no,) + slice_after]
-            output_array[red_band] = output_array[red_band] + rgb.red_intensity(data_at_spectra)
-            output_array[green_band] = output_array[green_band] + rgb.green_intensity(data_at_spectra)
-            output_array[blue_band] = output_array[blue_band] + rgb.blue_intensity(data_at_spectra)
-        scaled_to_rgb = (output_array / output_array.max()) * 255
-        return (scaled_to_rgb + 0.5).astype(int)
+    def iterate(self):
+        bands_range = range(self.__no_of_bands)
+        bands = reversed(bands_range) if self.__inverse else bands_range
+        return zip(self, bands)
+
--- SpectralData.py
+++ SpectralData.py
@@ -1,4 +1,5 @@
 from Colors import Colors
+import numpy as np
 
 
 class SpectralData:
@@ -12,4 +13,15 @@ class SpectralData:
         output_array_shape = self.__array.shape[:self.__axis] + (3,) + self.__array.shape[self.__axis + 1:]
         slice_before = (slice(None),) * len(self.__array.shape[:self.__axis])
         slice_after = (slice(None),) * len(self.__array.shape[self.__axis + 1:])
-        return self.__colors.convert_data(self.__array, output_array_shape, slice_before, slice_after)
+        output_array = np.zeros(output_array_shape)
+        red_band = slice_before + (0,) + slice_after
+        green_band = slice_before + (1,) + slice_after
+        blue_band = slice_before + (2,) + slice_after
+        for color, band_no in self.__colors.iterate():
+            rgb = color.rgb()
+            data_at_spectra = self.__array[slice_before + (band_no,) + slice_after]
+            output_array[red_band] = output_array[red_band] + rgb.red_intensity(data_at_spectra)
+            output_array[green_band] = output_array[green_band] + rgb.green_intensity(data_at_spectra)
+            output_array[blue_band] = output_array[blue_band] + rgb.blue_intensity(data_at_spectra)
+        scaled_to_rgb = (output_array / output_array.max()) * 255
+        return (scaled_to_rgb + 0.5).astype(int)
