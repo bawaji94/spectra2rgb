cd86bea4dca1d4b8e94182021cdab7ab966b4619
Shabbir Bawaji <bawaji94@gmail.com>
2023-01-04 16:39:38 +0530

    move inverse as data attribute

--- SpectralData.py
+++ SpectralData.py
@@ -2,14 +2,14 @@ from Colors import Colors
 
 
 class SpectralData:
-    def __init__(self, array, axis):
+    def __init__(self, array, axis, inverse=False):
         self.__array = array
         self.__axis = axis
-
-    def to_rgb(self, inverse=False):
         no_of_bands = self.__array.shape[self.__axis]
+        self.__colors = Colors(no_of_bands, inverse)
+
+    def to_rgb(self):
         output_array_shape = self.__array.shape[:self.__axis] + (3,) + self.__array.shape[self.__axis + 1:]
         slice_before = (slice(None),) * len(self.__array.shape[:self.__axis])
         slice_after = (slice(None),) * len(self.__array.shape[self.__axis + 1:])
-        colors = Colors(no_of_bands, inverse)
-        return colors.convert_data(self.__array, output_array_shape, slice_before, slice_after)
+        return self.__colors.convert_data(self.__array, output_array_shape, slice_before, slice_after)
