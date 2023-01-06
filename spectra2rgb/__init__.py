44bc241487d61c21a0a871b2c6911974bf81ad15
Shabbir Bawaji <bawaji94@gmail.com>
2023-01-06 16:28:09 +0530

    add SpectralData tests

--- spectra2rgb/__init__.py
+++ spectra2rgb/__init__.py
@@ -2,4 +2,4 @@ from spectra2rgb.RGB import *
 from spectra2rgb.Color import *
 from spectra2rgb.Colors import *
 from spectra2rgb.Slice import *
-from spectra2rgb.SpectralData import *
+from spectra2rgb.spectralData import *
--- test/TestSpectra2rgb.py
+++ test/TestSpectra2rgb.py
@@ -1,54 +1,38 @@
 import unittest
-from spectra2rgb import SpectralData
+from unittest import mock
 import numpy as np
+
+from spectra2rgb.spectralData import SpectralData
 
 
 class TesSpectra2RGB(unittest.TestCase):
-    def test_to_RGB_should_convert_multi_spectra_to_rgb(self):
-        array = np.arange(27).reshape(3, 3, 3)
+    @mock.patch("spectra2rgb.spectralData.Slice")
+    @mock.patch("spectra2rgb.spectralData.Colors")
+    def test_creation_of_spectral_data(self, mock_colors, mock_slice):
+        mock_colors.return_value = mock_colors
+        mock_slice.return_value = mock_slice
+        x = np.random.random((3, 3, 3))
 
-        expected = [[[135, 150, 165], [180, 195, 210], [225, 240, 255]],
-                    [[97, 108, 119], [130, 141, 152], [162, 173, 184]],
-                    [[0, 3, 6], [10, 13, 16], [19, 23, 26]]]
+        SpectralData(x, axis=0)
 
-        rgb = SpectralData(array, 0).to_rgb()
-        np.testing.assert_array_equal(expected, rgb)
+        mock_colors.assert_called_once()
+        mock_colors.assert_called_with(3, False)
+        mock_slice.assert_called_once()
+        mock_slice.assert_called_with((3, 3, 3), 0)
 
-    def test_to_RGB_should_convert_multi_spectra_inversed_to_rgb(self):
-        array = np.arange(27).reshape(3, 3, 3)
+    @mock.patch("spectra2rgb.spectralData.Slice")
+    @mock.patch("spectra2rgb.spectralData.Colors")
+    def test_conversion_to_rgb_values(self, mock_colors, mock_slice):
+        mock_colors.return_value = mock_colors
+        mock_slice.return_value = mock_slice
+        mock_colors.rgb_intensities.return_value = 'dummy_return_value'
+        x = np.random.random((3, 3, 3))
 
-        expected = [[[135, 150, 165], [180, 195, 210], [225, 240, 255]],
-                    [[97, 108, 119], [130, 141, 152], [162, 173, 184]],
-                    [[58, 62, 65], [68, 71, 75], [78, 81, 84]]]
+        actual = SpectralData(x, axis=0).to_rgb()
 
-        rgb = SpectralData(array, 0, inverse=True).to_rgb()
-        np.testing.assert_array_equal(expected, rgb)
-
-    def test_to_RGB_should_convert_single_spectra_to_red(self):
-        array = np.arange(9).reshape(3, 3, 1)
-
-        expected = [[[0, 0, 0], [32, 0, 0], [64, 0, 0]],
-                    [[96, 0, 0], [128, 0, 0], [159, 0, 0]],
-                    [[191, 0, 0], [223, 0, 0], [255, 0, 0]]]
-
-        rgb = SpectralData(array, 2, inverse=True).to_rgb()
-        np.testing.assert_array_equal(expected, rgb)
-
-    def test_to_RGB_should_not_convert_zero_band_data(self):
-        array = np.array([]).reshape(3, 3, 0)
-        with self.assertRaises(Exception) as context:
-            SpectralData(array, axis=2)
-        self.assertEqual("No of spectral bands should be > 0", str(context.exception))
-
-    def test_to_RGB_should_convert_multi_spectra_to_rgb_in_axis_1(self):
-        array = np.arange(9 * 7).reshape(3, 7, 3)
-
-        expected = [[[52, 57, 61], [28, 31, 34], [7, 10, 12]],
-                    [[149, 154, 158], [100, 103, 107], [58, 61, 63]],
-                    [[246, 250, 255], [172, 176, 179], [109, 112, 114]]]
-
-        rgb = SpectralData(array, 1).to_rgb()
-        np.testing.assert_array_equal(expected, rgb)
+        mock_colors.rgb_intensities.assert_called_once()
+        mock_colors.rgb_intensities.assert_called_with(x, mock_slice)
+        self.assertEqual(actual, 'dummy_return_value')
 
 
 if __name__ == '__main__':
