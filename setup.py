78fb16625de0878296d92ff7a4d338739df39dfa
Shabbir Bawaji <bawaji94@gmail.com>
2023-01-11 10:18:09 +0530

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='spectra2rgb',
+    version='1.0.0',
+    description='Converts multi-spectrum cube to RGB image',
+    author='Shabbir Nuruddin Bawaji',
+    author_email='bawaji94@gmail.com',
+    url='https://github.com/bawaji94/spectra2rgb',
+    packages=['spectra2rgb'],
+    install_requires=['numpy>=1.3.0']
+)
